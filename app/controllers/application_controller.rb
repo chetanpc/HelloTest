fd883ea324eb2f6cd3a573650e82280c99556c29
chetanpc <chetan.pc@gmail.com>
2015-03-19 23:56:50 -0700

    Version1.0

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  # config.autoload_paths += %W(#{config.root}/lib)
+  protect_from_forgery with: :exception
+
+  def helloaxn
+    render text: "hi there"
+  end
+end
